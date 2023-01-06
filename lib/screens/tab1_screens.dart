b0304876bc4598492ba575544b6ac0e7b15ae4eb
Jefry Sánchez <sanchezmorajeffrey@yahoo.com>
2023-01-05 19:34:17 -0600

    change type widget StatalessWidget to StateFulWidget

--- lib/screens/tab1_screens.dart
+++ lib/screens/tab1_screens.dart
@@ -3,12 +3,25 @@ import 'package:newsapp/services/news_service.dart';
 import 'package:newsapp/widgets/news_list.dart';
 import 'package:provider/provider.dart';
 
-class Tab1 extends StatelessWidget {
+class Tab1 extends StatefulWidget {
   const Tab1({super.key});
 
   @override
+  State<Tab1> createState() => _Tab1State();
+}
+
+class _Tab1State extends State<Tab1> with AutomaticKeepAliveClientMixin {
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
     final newsService = Provider.of<NewsService>(context);
-    return NewsList(news: newsService.headlines);
+    return (newsService.headlines.isEmpty)
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : NewsList(news: newsService.headlines);
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
